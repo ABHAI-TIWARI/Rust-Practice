7c67265d838afe31d8f350258741a2afb99c0321
ABHAI-TIWARI <abhaytab444@gmail.com>
2026-02-07 22:34:41 +0530

    print+

--- Projects/print_002/src/main.rs
+++ Projects/print_002/src/main.rs
@@ -14,4 +14,10 @@ fn main() {
     let path = r"C:\Users\John\Documents\file.txt";
     println!("The file is located at: {}", path);
 
+    let msg = format!(
+        "I am {name} and I am {age} years old"
+    );
+
+    println!("{}", msg);
+
 }
